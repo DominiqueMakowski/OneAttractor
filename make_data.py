4f69e8d8b3b436fbe6821a3e02cd9d9179b769a7
Dominique Makowski <dom.mak19@gmail.com>
2022-02-02 13:40:07 +0800

    be consistent

--- make_data.py
+++ make_data.py
@@ -12,7 +12,7 @@ emb = nk.complexity_embedding(
     a=1 + np.log(d.day),
     b=1 + np.log(d.month),
     c=2.0,  # If you're reading this after year 2100, hello from the past and please change this to 2.1 thanks :)
-    d=(d.year - 2000) / 10,  # Last 2 digits of year
+    d=1 + np.log(d.year - 2000),  # Last 2 digits of year
 )
 
 pd.DataFrame(emb, columns=["x", "y"]).to_csv("data.csv", index=False)
