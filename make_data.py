e289fe949af7ec6051d5b2e8f85ef6cd887b0726
Dominique Makowski <dom.mak19@gmail.com>
2022-02-02 13:37:50 +0800

    change the formula for nicer results

--- make_data.py
+++ make_data.py
@@ -1,6 +1,7 @@
 import datetime
 
 import neurokit2 as nk
+import numpy as np
 import pandas as pd
 
 d = datetime.datetime.now()
@@ -8,8 +9,8 @@ d = datetime.datetime.now()
 emb = nk.complexity_embedding(
     "clifford",
     sampling_rate=1000000,
-    a=d.day / 10,
-    b=d.month / 10,
+    a=1 + np.log(d.day),
+    b=1 + np.log(d.month),
     c=2.0,  # If you're reading this after year 2100, hello from the past and please change this to 2.1 thanks :)
     d=(d.year - 2000) / 10,  # Last 2 digits of year
 )
